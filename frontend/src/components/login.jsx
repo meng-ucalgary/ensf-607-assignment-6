9ed71a37f774133de51a249b85f5cbb36229d2c6
Sarang K <sarang3@gmail.com>
2021-12-02 23:03:24 -0700

    build: [U888-63] User Authentication in progres

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,10 +1,103 @@
 
 
-import React, { Component } from 'react';
+import React, { useState, Component } from 'react';
 import NavBarStart from './navbarstart';
+import {getUsers} from './../services/fakeUserService';  
+import { useHistory } from 'react-router-dom';
 class Login extends React.Component {
+    
+    state = {
+        users:getUsers(),
+        username: "",
+        password: "",
+        errorMessage: "",
+        redirect:null
+       // verifyUser: false,
+    };
+
+   
+    getUsername = (e) => {
+        console.log('Username: ', e.target.value)
+        this.setState({username: e.target.value})
+    }
+
+    getPassword = (e) => {
+        console.log('Password: ', e.target.value)
+        this.setState({password: e.target.value})
+    }
+
+    //verifyUser = getUsers().map(users => {
+    /*
+    verifyUser = (e) => {
+        
+        for (var i=0; i<this.state.users.length; i++) {
+           
+            //console.log(this.state.users[1]["emailId"])
+        
+            //role 0 is for instructor
+            if ((this.state.username == this.state.users[i]["emailId"]) && (this.state.users[i]["password"] == this.state.users[i]["u_passwordhash"]) && (this.state.users[i]["role"] = "0")) {
+                console.log('Successful Login1');
+                //<a href="/i/menu" class="btn btn-primary"/>
+                //this.setState({ redirect: "/i/menu" });
+                
+            }
+
+            //role 1 is for admin
+            else if ((this.state.username == this.state.users["emailId"]) && (this.state.users["password"] == this.state.users["u_passwordhash"]) && (this.state.users[i]["role"] = "1")) {
+                console.log('Successful Login');
+                <a href="/a/menu" class="btn btn-primary"/>
+            }
+
+            //role 2 is for technician
+            else if ((this.state.username == this.state.users[i]["emailId"]) && (this.state.users[i]["password"] == this.state.users["u_passwordhash"]) && (this.state.users[i]["role"] = "2")) {   
+                console.log('Successful Login');
+                <a href="/t/menu" class="btn btn-primary"/>
+            }
+
+            // else {
+            //     console.log("Incorrect credentials");
+            //     this.setState({errorMessage: "Incorrect credentials"});
+            // }
+            
+
+        }
+    }
+    */
+    
+    
     render() { 
-        console.log("Hi");
+        let hyperlink = ""
+
+        for (var i=0; i<this.state.users.length; i++) {
+           
+            //console.log(this.state.users[1]["emailId"])
+        
+            //role 0 is for instructor
+            if ((this.state.username == this.state.users[i]["emailId"]) && (this.state.users[i]["password"] == this.state.users[i]["u_passwordhash"]) && (this.state.users[i]["role"] = "0")) {
+                console.log('Successful Login1');
+                hyperlink = "/i/menu" 
+            }
+
+            //role 1 is for admin
+            else if ((this.state.username == this.state.users["emailId"]) && (this.state.users["password"] == this.state.users["u_passwordhash"]) && (this.state.users[i]["role"] = "1")) {
+                console.log('Successful Login2');
+                hyperlink = "/a/menu" 
+            }
+
+            //role 2 is for technician
+            else if ((this.state.username == this.state.users[i]["emailId"]) && (this.state.users[i]["password"] == this.state.users["u_passwordhash"]) && (this.state.users[i]["role"] = "2")) {   
+                console.log('Successful Login3');
+                hyperlink = "/t/menu" 
+            }
+
+            // else {
+            //     console.log("Incorrect credentials");
+            //     this.setState({errorMessage: "Incorrect credentials"});
+            // }
+            
+
+        }
+ 
         return <React.Fragment>
             <NavBarStart/>
             <div class="container">
@@ -19,16 +112,21 @@ class Login extends React.Component {
                             
                                 <form>
                                     <div class="form-group">
-                                        <label class="form-control-label">Username</label>
-                                        <input type="text" class="form-control"/>
+                                        <label class="form-control-label" >Username</label>
+                                        <input type="text" class="form-control"onChange = {(e) => this.getUsername(e)}/>
                                     </div>
                                     <div class="form-group">
                                         <label class="form-control-label">Password</label>
-                                        <input type="password" class="form-control" i/>
+                                        <input type="password" class="form-control" i onChange = {(e) => this.getPassword(e)}/>
                                     </div>
-
+                                    {/* <div class="error">
+                                        <h3>  { this.state.errorMessage } </h3> 
+                                    </div> */}
                                     <div >
-                                        <a href="/t/menu" class="btn btn-primary">Login</a>
+                                        <a href = {hyperlink} class="btn btn-primary">Login</a>
+                                        {/* <a href="/menu" class="btn btn-primary">Login</a> */}
+                                        {/* <a  class="btn btn-primary" onclick = {(e) => this.verifyUser(e)}>Login</a> */}
+                                        {/* <button type="submit" class="btn btn-primary" onClick = {(e) => this.verifyUser(e)} >Login</button> */}
                                         <button type="submit" class="btn btn-outline-primary">Forget Password</button>
                                         
                                     </div>
