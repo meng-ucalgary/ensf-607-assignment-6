447f0ad486a05f27aba10ddd63f5d17b6d6d185e
Bhavyai Gupta <zbhavyai@gmail.com>
2021-12-03 03:12:17 -0700

    build: [U888-53, U888-16, U888-25, U888-40] connecting comments to db

--- frontend/src/components/commentlist.jsx
+++ frontend/src/components/commentlist.jsx
@@ -1,95 +1,85 @@
 import React, { Component } from 'react';
 import axios from "axios";
-import {getAnimalbyId} from '../services/fakeAnimalsService'; 
 import NavBar from './navbar';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Link
-  } from "react-router-dom";
+} from "react-router-dom";
+
+
 class CommentList extends React.Component {
     state = {
         animal: {},
-        
-        
+        comments: [],
         imageUrl: 'https://picsum.photos/200',
-
-        
-        
-        
     };
 
     async componentDidMount() {
         const id = this.props.match.params.id;
-        const {data: posts} = await axios.get('http://localhost:8080/api/v1/animals/'+{id}, {headers: {'Access-Control-Allow-Origin': true,},});
-        
-        this.setState({posts});
-        
 
-        
+        var animalIdUrl = "http://localhost:8080/api/v1/animals/" + id
+        const { data: animal } = await axios.get(animalIdUrl, { headers: { 'Access-Control-Allow-Origin': true, }, });
+        this.setState({ animal });
 
-        
-            
-        //const promise = axios.get('https://jsonplaceholder.typicode.com/posts')
-        
+        var animalCommentUrl = "http://localhost:8080/api/v1/animals/" + id + "/comments"
+        const { data: comments } = await axios.get(animalCommentUrl, { headers: { 'Access-Control-Allow-Origin': true, }, });
+        this.setState({ comments });
     }
 
 
-    componentWillMount() {
-        const id = this.props.match.params.id;
-        console.log(id);
+    // componentWillMount() {
+    //     const id = this.props.match.params.id;
+    //     // console.log(id);
 
+    //     this.setState({
+    //         animal: getAnimalbyId(id),
+    //     });
+    // }
 
-        this.setState({animal: getAnimalbyId(id), 
-            
-        });
+    render() {
+        // const user = this.props.match.params.user;
 
- }
-
-    render() { 
-        const user = this.props.match.params.user;
-        console.log(this.state.animal["animalCommentList"]);
 
         return <React.Fragment>
-            <NavBar user = {user}/>
+            {/* <NavBar user={user} /> */}
+            <NavBar />
             <h2 class="display-4">Comment Logs</h2>
             <div class="row">
-                    <div class="col-sm">
-                        <img src={this.state.imageUrl} alt=""/>
+                <div class="col-sm">
+                    <img src={this.state.imageUrl} alt="" />
 
-                    </div>
-                    <div class="col-sm">
-                        <div class="jumbotron jumbotron-fluid">
-                            <div class="container">
-                                <h1 class="display-4">Basic Details</h1>
-                                <p class="lead">Animal ID: {this.state.animal["animalId"]}</p>
+                </div>
+                <div class="col-sm">
+                    <div class="jumbotron jumbotron-fluid">
+                        <div class="container">
+                            <h1 class="display-4">Basic Details</h1>
+                            <p class="lead">Animal ID: {this.state.animal["animalId"]}</p>
                                 <p class="lead">Name: {this.state.animal["name"]}</p>
                                 <p class="lead">Breed: {this.state.animal["breed"]}</p>
                                 <p class="lead">Age: {this.state.animal["age"]}</p>
-                                <p class="lead">Sex: {this.state.animal["animalId"]}</p>
-                                <p class="lead">Status: {this.state.animal["sex"]}</p>
-                            </div>
+                                <p class="lead">Sex: {this.state.animal["sex"]}</p>
+                                <p class="lead">Status: {this.state.animal["status"]}</p>
                         </div>
                     </div>
-                    
+                </div>
+
             </div>
 
             <div class="row">
                 <div class="jumbotron jumbotron-fluid">
                     <div class="container">
                         <h1 class="display-4">Log History</h1>
-                        {this.state.animal["animalCommentList"].map(comment =>(
+                        {this.state.comments.map(comment => (
                             <div class="card" style={this.styles}>
-                            <p>Log {comment["commentid"]}: {comment["date"]}</p>
-                            
-                            <p>{comment["commentdesc"]}</p>
-                            <td><Link to={"/animals/"+this.state.animal["animalId"].toString()} className="btn btn-secondary btn-sm">Edit</Link><Link to={"/animals/"+this.state.animal["animalId"].toString()} className="btn btn-secondary btn-sm">Remove</Link></td>
-                            <p></p>
-                            <p></p>
-                            </div>
-                            
+                                <p>Log {comment["commentId"]}: {comment["commentDate"]}</p>
 
+                                <p>{comment["commentDesc"]}</p>
+                                <td><Link to={"/animals/" + this.state.animal["animalId"].toString()} className="btn btn-secondary btn-sm">Edit</Link><Link to={"/animals/" + this.state.animal["animalId"].toString()} className="btn btn-secondary btn-sm">Remove</Link></td>
+                                <p></p>
+                                <p></p>
+                            </div>
                         ))}
                     </div>
                 </div>
@@ -98,5 +88,5 @@ class CommentList extends React.Component {
         </React.Fragment>;
     }
 }
- 
+
 export default CommentList;
