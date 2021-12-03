90f01340813ee9ec9cdf71d644bf7dec12801f5f
mlee2021 <83717300+mlee2021@users.noreply.github.com>
2021-12-03 11:43:07 -0700

    fix:[U888-63] routing issues

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import reactDom from "react-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import { BrowserRouter } from "react-router-dom";
+
 import 'font-awesome/css/font-awesome.css';
 import './index.css';
 import App from "./App";
 import AnimalManagement from "./components/animalmanagement";
+
 import Login from "./components/login";
-import { Route, Switch } from 'react-router-dom';
+
+import {Route, Switch} from 'react-router-dom';
 import { Routes } from 'react-router';
 import NavBar from "./components/navbar";
 import AnimalProfile from "./components/animalprofile";
@@ -21,27 +24,36 @@ import MainMenu from "./components/mainmenu";
 import axios from "axios";
 import ErrorPage from './components/errorpage';
 import HomePage from './components/homepage';
-import CommentList from "./components/commentlist";
-import IssueList from "./components/issuelist";
-import TreatmentList from "./components/treatmentlist";
+import CommentList from './components/commentlist';
+import RequestSubmission from './components/submitrequest';
+import RequestManagement from "./components/requestmanagement";
 
 
 
 reactDom.render(
-    <BrowserRouter>
-        <Switch>
-            <Route path='/users/:id' component={UserDetails} />
-            <Route path="/animals/:id/comments" component={CommentList} />
-            <Route path="/animals/:id/issues" component={IssueList} />
-            <Route path="/animals/:id/treatments" component={TreatmentList} />
-            <Route path="/animals/:id" component={AnimalProfile} />
-            <Route path="/animals" component={AnimalManagement} />
-            <Route path="/users" component={UserManagement} />
-            <Route path="/login" component={Login} />
-            <Route path='/register' component={RegistrationPage} />
-            <Route path='/youraccount' component={YourAccount} />
-            <Route path="/menu" component={MainMenu} />
-            <Route path="/" component={HomePage} />
-            <Route path='*' component={ErrorPage} />
-        </Switch>
-    </BrowserRouter>, document.getElementById('root'));
+
+
+<BrowserRouter>
+
+
+    <Switch>
+    <Route path='/:user/animals/:id/comments' component={CommentList}/>
+    <Route path='/:user/requestmanagment' component={RequestManagement}/>
+    <Route path='/:user/requestsubmission' component={RequestSubmission}/>
+      <Route path='/:user/users/:id' component={UserDetails}/>
+      <Route path="/:user/animals/:id" component={AnimalProfile} />
+      <Route path="/:user/animals" component={AnimalManagement}/>
+      <Route path="/login" component={Login}/>
+      <Route path="/:user/users" component={UserManagement}/>
+      
+      <Route path='/register' component={RegistrationPage}/>
+      <Route path='/:user/youraccount' component={YourAccount}/>
+      <Route path="/:user/menu" component={MainMenu} />
+      <Route path="/" component={HomePage} />
+      <Route path='*' component={ErrorPage}/>
+      </Switch>
+    
+
+
+</BrowserRouter>,document.getElementById('root'));
+
