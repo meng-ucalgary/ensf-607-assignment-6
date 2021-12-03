5dc0e8f6233d1f57c768c0b6a812b69d9f5f7b27
Bhavyai Gupta <zbhavyai@gmail.com>
2021-12-03 03:14:12 -0700

    build: [U888-63] updating router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,12 @@ import reactDom from "react-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import { BrowserRouter } from "react-router-dom";
-
 import 'font-awesome/css/font-awesome.css';
 import './index.css';
 import App from "./App";
 import AnimalManagement from "./components/animalmanagement";
-
 import Login from "./components/login";
-
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { Routes } from 'react-router';
 import NavBar from "./components/navbar";
 import AnimalProfile from "./components/animalprofile";
@@ -24,31 +21,27 @@ import MainMenu from "./components/mainmenu";
 import axios from "axios";
 import ErrorPage from './components/errorpage';
 import HomePage from './components/homepage';
+import CommentList from "./components/commentlist";
+import IssueList from "./components/issuelist";
+import TreatmentList from "./components/treatmentlist";
 
 
 
 reactDom.render(
-
-
-<BrowserRouter>
-
-
-    <Switch>
-      
-      <Route path='/users/:id' component={UserDetails}/>
-      <Route path="/animals/:id" component={AnimalProfile} />
-      <Route path="/animals" component={AnimalManagement}/>
-      <Route path="/login" component={Login}/>
-      <Route path="/users" component={UserManagement}/>
-      <Route path="/login" component={Login}/>
-      <Route path='/register' component={RegistrationPage}/>
-      <Route path='/youraccount' component={YourAccount}/>
-      <Route path="/menu" component={MainMenu} />
-      <Route path="/" component={HomePage} />
-      <Route path='*' component={ErrorPage}/>
-      </Switch>
-    
-
-
-</BrowserRouter>,document.getElementById('root'));
-
+    <BrowserRouter>
+        <Switch>
+            <Route path='/users/:id' component={UserDetails} />
+            <Route path="/animals/:id/comments" component={CommentList} />
+            <Route path="/animals/:id/issues" component={IssueList} />
+            <Route path="/animals/:id/treatments" component={TreatmentList} />
+            <Route path="/animals/:id" component={AnimalProfile} />
+            <Route path="/animals" component={AnimalManagement} />
+            <Route path="/users" component={UserManagement} />
+            <Route path="/login" component={Login} />
+            <Route path='/register' component={RegistrationPage} />
+            <Route path='/youraccount' component={YourAccount} />
+            <Route path="/menu" component={MainMenu} />
+            <Route path="/" component={HomePage} />
+            <Route path='*' component={ErrorPage} />
+        </Switch>
+    </BrowserRouter>, document.getElementById('root'));
